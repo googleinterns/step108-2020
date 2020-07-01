59d6c30aa6309203d2cad7661dd4b844dbec5182
brandonkj23 <brandonkj2001@yahoo.com>
2020-07-01 15:38:03 -0400

    add ball.ai

---
+++ ball.ai/src/main/java/com/google/sps/servlets/PlayerInfoServlet.java
@@ -0,0 +1,37 @@
+package com.google.sps.servlets;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.List;
+import java.net.URL;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/playerInfo")
+public class PlayerInfoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        String fileName = "players.csv";
+        File file = new File(fileName);
+        try{
+            Scanner inputStream = new Scanner(file);
+            while(inputStream.hasNext()){
+                String data = inputStream.next();
+                response.getWriter().println(data);
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        
+    }
+}
