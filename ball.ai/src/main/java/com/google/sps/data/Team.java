f8487c2d09811e1e17c9e36d9412ad657dcb22b4
jeremy-weiss <j.weiss@yale.edu>
2020-07-13 10:44:38 -0400

    Add team_id to Team class

--- ball.ai/src/main/java/com/google/sps/data/Team.java
+++ ball.ai/src/main/java/com/google/sps/data/Team.java
@@ -4,12 +4,13 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 public class Team {
-  public String team_name, team_nickname, team_abbrev, division_id, conference;
+  public String team_name, team_nickname, team_abbrev, team_id, division_id, conference;
 
-  public Team(String city, String name, String abbrv, String division, String conference) {
+  public Team(String city, String name, String abbrv, String id, String division, String conference) {
     this.team_name = city;
     this.team_nickname = name;
     this.team_abbrev = abbrv;
+    this.team_id = id;
     this.division_id = division;
     this.conference = conference;
   }
