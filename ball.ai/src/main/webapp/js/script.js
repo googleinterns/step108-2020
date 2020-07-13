ab87dad12b11b0d2ea6d86f6856683bd02f15ae6
brandonkj23 <brandonkj2001@yahoo.com>
2020-07-13 11:05:23 -0400

    add choosePlayer and createPlayersArray functions

--- ball.ai/src/main/webapp/js/script.js
+++ ball.ai/src/main/webapp/js/script.js
@@ -20,11 +20,14 @@ function searchFunction() {
   }
 }
 
-
+//draws a chart of players selected by the user
 function drawChart(){
   fetch('/playerInfo').then(response => response.json()).then((team) => {
+    text = localStorage.getItem("playersArray");
+    selectedPlayers = JSON.parse(text);
     const players = Object.values(team);
     var data = new google.visualization.DataTable();
+    var counter = 1;
     for(i=0;i < players.length;i++){
       if(i==0){
         data.addColumn('string','Stats');
@@ -33,10 +36,18 @@ function drawChart(){
         data.setCell(1,0,'REB');
         data.setCell(2,0,'STL');
       }
-      data.addColumn('number',players[i].name);
-      data.setCell(0,i+1,players[i].points);
-      data.setCell(1,i+1,players[i].rebounds);
-      data.setCell(2,i+1,players[i].steals);
+      console.log("hi");
+      for(j=0; j < selectedPlayers.length; j++){
+        console.log(selectedPlayers[j].name);
+        if(selectedPlayers[j].name==players[i].name && 
+          selectedPlayers[j].year==players[i].year){
+          data.addColumn('number',players[i].name);
+          data.setCell(0,counter,players[i].points);
+          data.setCell(1,counter,players[i].rebounds);
+          data.setCell(2,counter,players[i].steals);
+          counter++;
+        }
+      }
     }
     var options = {
       colors:['gray'],
@@ -51,5 +62,36 @@ function drawChart(){
 
     chart.draw(data, google.charts.Bar.convertOptions(options));
   });
+}
+
+//adds a player to the area in storage
+function choosePlayer(id){
+  text = localStorage.getItem("playersArray");
+  var players = JSON.parse(text);
+  player ={
+    name: document.getElementById('player-input'+id).value,
+    year: document.getElementById('year-input'+id).value,
+  }
+  if(players[0].name==""){
+    players[0]=player;
+  }else{
+    players.push(player);
+  }
+  console.log(player.name);
+  var playerJSON = JSON.stringify(players);
+  localStorage.setItem("playersArray",playerJSON);
+  drawChart();
+}
+
+//creates an empty array of players and stores it
+function createPlayersArray(){
+  var players = new Array();
+  player ={
+    name: "",
+    year: "",
+  }
+  players.push(player);
+  var playerJSON = JSON.stringify(players);
+  localStorage.setItem("playersArray",playerJSON);
 }
 
