7c97df76a857217f612fdd6fbccd238e6d241e06
brandonkj23 <brandonkj2001@yahoo.com>
2020-07-15 13:19:30 -0400

    add working search bar

--- ball.ai/src/main/webapp/js/script.js
+++ ball.ai/src/main/webapp/js/script.js
@@ -1,25 +1,27 @@
 google.charts.load('current', {'packages':['bar']});
 google.charts.setOnLoadCallback(drawChart);
 
-function searchFunction() {
+const selectedPlayers = new Array();
+
+function search(){
   fetch('/search').then(response => response.json()).then((players) => {
-    const playersList = new Map(Object.entries(players));
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchQuery');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("listPlayers");
-    li = ul.getElementsByTagName('li');
+    const listPlayers = Object.keys(players);
+    const seasonsPlayed = new Map(Object.entries(players));
+    input = document.getElementById('searchQuery').value;
+    document.getElementById('list').innerHTML= '';
+    counter = 0;
+    for(i=0; i < listPlayers.length;i++){
+      if(((listPlayers[i].toLowerCase()).indexOf(input.toLowerCase()))>-1){
 
+        var node = document.createElement("option"); 
+        var val = document.createTextNode(listPlayers[i]); 
+        node.appendChild(val); 
 
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
+        document.getElementById('list').appendChild(node); 
+        counter++;
+        if(counter >6){
+          break;
+        }
       }
     }
   });
@@ -28,8 +30,6 @@ function searchFunction() {
 //draws a chart of players selected by the user
 function drawChart(){
   fetch('/playerInfo').then(response => response.json()).then((team) => {
-    text = localStorage.getItem("playersArray");
-    selectedPlayers = JSON.parse(text);
     const players = new Map(Object.entries(team));
     var data = new google.visualization.DataTable();
     var counter = 1;
@@ -67,32 +67,15 @@ function drawChart(){
 
 //adds a player to the area in storage
 function choosePlayer(id){
-  text = localStorage.getItem("playersArray");
-  var players = JSON.parse(text);
   var name = document.getElementById('player-input'+id).value;
   var season = document.getElementById('year-input'+id).value;
   player ={
     id: name+season,
   }
-  if(players[0].id==""){
-    players[0]=player;
-  }else{
-    players.push(player);
-  }
-  console.log(player.name);
-  var playerJSON = JSON.stringify(players);
-  localStorage.setItem("playersArray",playerJSON);
+  selectedPlayers.push(player);
+  console.log(player.id);
   drawChart();
 }
 
-//creates an empty array of players and stores it
-function createPlayersArray(){
-  var players = new Array();
-  player ={
-    id: "",
-  }
-  players.push(player);
-  var playerJSON = JSON.stringify(players);
-  localStorage.setItem("playersArray",playerJSON);
-}
+
 
