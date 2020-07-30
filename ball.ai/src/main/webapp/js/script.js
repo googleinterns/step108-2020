f0f6e333ed51435dc5e6847a3d82ef2d2af24ee5
brandonkj23 <brandonkj2001@yahoo.com>
2020-07-30 09:55:44 -0400

    can choose players positions from what they played

--- ball.ai/src/main/webapp/js/script.js
+++ ball.ai/src/main/webapp/js/script.js
@@ -1,12 +1,11 @@
 google.charts.load('current', {'packages':['bar']});
 
 const selectedPlayers = new Array();
-var listId = 0; //id for datalist of seasons played for the next player selected
 
 /**
  * @return object of players and lists of years they played
  */
-async function getPlayers(){
+async function getPlayers() {
   const response = await fetch('/search');
   const players = await response.json();
   return players;
@@ -15,21 +14,21 @@ async function getPlayers(){
 /**
  * @return current input in searchQuery
  */
-function getInput(){
+function getInput() {
   return document.getElementById('searchQuery').value;
 }
 
 /**
  * Auto completes users input by providing options to select based on their input
  */
-async function search(){
+async function search() {
   document.getElementById('list').innerHTML = '';
   const players = await getPlayers();
   const listPlayers = Object.keys(players);
   input = getInput();
   counter = 0;
-  for(i=0; i < listPlayers.length;i++){
-    if(((listPlayers[i].toLowerCase()).indexOf(input.toLowerCase())) > - 1 && counter < 6){
+  for (i = 0; i < listPlayers.length; i++){
+    if (((listPlayers[i].toLowerCase()).indexOf(input.toLowerCase())) > - 1 && counter < 6) {
       var node = document.createElement("option"); 
       var val = document.createTextNode(listPlayers[i]); 
       node.appendChild(val); 
@@ -42,7 +41,7 @@ async function search(){
 /**
  * creates an element of the players chosen from search bar
  */
-async function selectPlayer(){
+async function selectPlayer() {
   const players = await getPlayers();
   const playerInfo = new Map(Object.entries(players));
   input = getInput();
@@ -59,11 +58,6 @@ async function selectPlayer(){
   deletePlayerButton.value = 'Remove Player';
   const position = document.createElement('select');
   position.id = 'positions';
-  position.innerHTML ='<option>PG</option>'+
-                      '<option>SG</option>'+
-                      '<option>SF</option>'+
-                      '<option>PF</option>'+
-                      '<option>C</option>';
 
   //button adds the selected player to the chart
   addPlayerButton.addEventListener('click',() => {
@@ -82,12 +76,33 @@ async function selectPlayer(){
   });
 
   //adds seasons the player played to seasonsList
-  for(j=0; j< playerInfo.get(input).seasonsPlayed.length;j++){
+  for ( j = 0; j < playerInfo.get(input).seasonsPlayed.length; j++) {
     var option = document.createElement("option");
-    var value = document.createTextNode(playerInfo.get(input).seasonsPlayed[j])
+    var value = document.createTextNode(playerInfo.get(input).seasonsPlayed[j]);
     option.appendChild(value);
     seasonsList.appendChild(option);
   }
+
+  for (i = 0; i < playerInfo.get(input).positionsPlayed.length; i++) {
+    var option1 = document.createElement("option");
+    var option2 = document.createElement("option");
+    var pos = playerInfo.get(input).positionsPlayed[i];
+    if (pos == 'G') {
+      option1.innerText = 'P' + pos;
+      option2.innerText = 'S' + pos;
+      position.appendChild(option1);
+      position.appendChild(option2);
+    } else if (pos == 'F') {
+      option1.innerText = 'S' + pos;
+      option2.innerText = 'P' + pos;
+      position.appendChild(option1);
+      position.appendChild(option2);
+    } else {
+      option1.innerText = pos;
+      position.appendChild(option1);
+    }
+  }
+
   liElement.appendChild(seasonsList);
   liElement.appendChild(position);
   liElement.appendChild(addPlayerButton);
@@ -102,11 +117,11 @@ async function selectPlayer(){
  * @param {id of the current player} playerid 
  * @param {position selected for the player} pos 
  */
-function createPlayernode(name,imageId,playerid,position){
-  var container  = document.getElementById(''+position+'-container');
+function createPlayernode(name,imageId,playerid,position) {
+  var container  = document.getElementById('' + position + '-container');
   var player = document.createElement('li');
   player.innerText = name;
-  var url = "/images/downsize_player/"+imageId+".jpg"
+  var url = "/images/downsize_player/" + imageId + ".jpg"
   var img = document.createElement('img');
   img.height = "60";
   player.appendChild(img);
@@ -117,9 +132,10 @@ function createPlayernode(name,imageId,playerid,position){
   //removes player from chart
   deleteButton.addEventListener('click',() => {
     player.remove();
-    for(i = 0; i < selectedPlayers.length;i++){
-      if(selectedPlayers[i].id == playerid){
-        selectedPlayers[i].id = selectedPlayers[selectedPlayers.length-1].id;
+    size = selectedPlayers.length;
+    for (i = 0; i < size ; i++) {
+      if (selectedPlayers[i].id == playerid) {
+        selectedPlayers[i].id = selectedPlayers[size-1].id;
         selectedPlayers.pop();
       }
     }
@@ -131,29 +147,29 @@ function createPlayernode(name,imageId,playerid,position){
 }
 
 //draws a chart of players selected by the user
-function drawChart(){
+function drawChart() {
   fetch('/playerInfo').then(response => response.json()).then((team) => {
     const players = new Map(Object.entries(team));
     var data = new google.visualization.DataTable();
     var counter = 1;
     //creates a data table of the selected players and their stats
-    for(i = 0; i < selectedPlayers.length; i++){
-      if( i == 0){
-        data.addColumn('string','Stats');
+    for (i = 0; i < selectedPlayers.length; i++) {
+      if ( i == 0) {
+        data.addColumn('string', 'Stats');
         data.addRows(5);
-        data.setCell(0,0,'PPG');
-        data.setCell(1,0,'AST');
-        data.setCell(2,0,'REB');
-        data.setCell(3,0,'STL');
-        data.setCell(4,0,'BLK');
+        data.setCell(0, 0, 'PPG');
+        data.setCell(1, 0, 'AST');
+        data.setCell(2, 0, 'REB');
+        data.setCell(3, 0, 'STL');
+        data.setCell(4, 0, 'BLK');
       }
-      if(players.has(selectedPlayers[i].id)){
-        data.addColumn('number',players.get(selectedPlayers[i].id).name);
-        data.setCell(0,counter,players.get(selectedPlayers[i].id).points);
-        data.setCell(1,counter,players.get(selectedPlayers[i].id).assists);
-        data.setCell(2,counter,players.get(selectedPlayers[i].id).rebounds);
-        data.setCell(3,counter,players.get(selectedPlayers[i].id).steals);
-        data.setCell(4,counter,players.get(selectedPlayers[i].id).blocks);
+      if (players.has(selectedPlayers[i].id)) {
+        data.addColumn('number', players.get(selectedPlayers[i].id).name);
+        data.setCell(0, counter, players.get(selectedPlayers[i].id).points);
+        data.setCell(1, counter, players.get(selectedPlayers[i].id).assists);
+        data.setCell(2, counter, players.get(selectedPlayers[i].id).rebounds);
+        data.setCell(3, counter, players.get(selectedPlayers[i].id).steals);
+        data.setCell(4, counter, players.get(selectedPlayers[i].id).blocks);
         counter++;
       }
     }
