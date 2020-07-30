906728a42fda672f3dca7151d57f9fa2124e1b0b
brandonkj23 <brandonkj2001@yahoo.com>
2020-07-30 09:51:50 -0400

    add json to project

--- ball.ai/src/main/java/com/google/sps/servlets/SearchServlet.java
+++ ball.ai/src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -1,6 +1,9 @@
 package com.google.sps.servlets;
 
 import com.google.sps.data.PlayerInfo;
+import com.google.gson.reflect.TypeToken;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,6 +16,8 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.*;
 import java.lang.Double;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
 
 import com.google.gson.Gson;
 
@@ -30,34 +35,47 @@ public class SearchServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         BufferedReader br = null;
-        try{
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+
+            // create a reader
+            BufferedReader reader = new BufferedReader(new FileReader("resources/player_positions.json"));
+
+            // convert JSON file to map
+            Type type = new TypeToken<HashMap<String, List<String>>>(){}.getType();
+            HashMap<String, List<String>> map = gson.fromJson(reader, type);
+
+            // close reader
+            reader.close();
+
             br = new BufferedReader(new FileReader("resources/player-seasons_per_game.csv"));
-            CSVParser records = CSVFormat.DEFAULT
-                .withFirstRecordAsHeader()
-                .parse(br);
+            CSVParser records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(br);
             Map<String, PlayerInfo> listPlayers = new HashMap<>();
-            for (CSVRecord record: records){
+            for (CSVRecord record: records) {
                 String name = record.get("PLAYER_NAME");
                 int year = Integer.parseInt(record.get("SEASON"));
                 int player_id = Integer.parseInt(record.get("PLAYER_ID"));
                 
-                if(listPlayers.containsKey(name)){
-                    listPlayers.get(name).addYear(year);
-                }else{
-                    listPlayers.putIfAbsent(name, new PlayerInfo(name,player_id,year));
+                if (map.containsKey(name) && year > 2001) {
+
+                    if (listPlayers.containsKey(name)) {
+                        listPlayers.get(name).addYear(year);
+                    } else {
+                        listPlayers.putIfAbsent(name, new PlayerInfo(name, player_id, year, map.get(name)));
+                    }
                 }
             }
-            Gson gson = new Gson();
             response.setContentType("application/json;");
             response.getWriter().println(gson.toJson(listPlayers));
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try{
-                if(br != null){
+            try {
+                if (br != null) {
                     br.close();
                 }
-            }catch (IOException ex){
+            } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
