936a3c2e86f9b0b75155db54ee4ecb1c2425201b
jeremy-weiss <j.weiss@yale.edu>
2020-08-03 10:24:57 -0400

    Change filter system to brute force

--- ball.ai/src/main/webapp/scheduler/home-away/calendar.js
+++ ball.ai/src/main/webapp/scheduler/home-away/calendar.js
@@ -1,7 +1,12 @@
 // ?players={}
 
+var url = new URL(window.location.href);
+var urlTeam = url.searchParams.get("team");
+
 const firstDay = d3.timeDay(new Date("2015-10-28"));
 const gamesPerTeam = 82;
+const emptySet = new Set();
+let currentTeam;
 
 const svgDiv = document.getElementById("svgContainer");
 const collapseDiv = document.getElementById('filterCollapse');
@@ -18,18 +23,24 @@ const svg = d3.select(svgDiv).append("svg");
 const collapse = d3.select(collapseDiv).append("svg");
 
 let colorFn = d3.scaleSequential(d3.interpolateBuGn);
+const formatDate = d3.utcFormat("%x");
 
 let teams = [];
 let teamAbbrv = {};
 let days = 0;
-let workingSet;
+let workingSetAll;
 let scheduleData;
 let finalSchedule;
 d3.csv("/resources/teams.csv").then(data => {
+    let validUrlTeam = false;
     data.forEach(d => {
+        if (urlTeam === d["team_abbrev"]) {
+            validUrlTeam = true;
+        }
         teams.push({"abbrv": d["team_abbrev"], "name": `${d["team_name"]} ${d["team_nickname"]}`});
         teamAbbrv[d["team_abbrev"]] = `${d["team_name"]} ${d["team_nickname"]}`;
     });
+    currentTeam = validUrlTeam ? teams.filter(t => t.abbrv === urlTeam)[0] : teams[0];
     loadSchedule();
 });
 
@@ -45,17 +56,24 @@ function loadSchedule() {
         days++;
 
         // Count number of games per day
-        let schedule = [];
-        for (let i = 0; i < days; i++) {
-            const date = d3.timeDay.offset(firstDay, i);
-            schedule.push({"date": date, "value": data.filter(d => d.day === i).length});
-        }
+        const schedule = daySchedule(data, emptySet);
         scheduleData = data;
-        workingSet = new Set(data);
+        workingSetAll = new Set(data);
         finalSchedule = schedule;
         drawFilter(drawCalendar());
         // drawTeamSchedule(teams[0]);
     });
+}
+
+// Could save memory by separating date and value + speed up w/o filter() but it's not significant
+function daySchedule(data, teams) {
+    let schedule = [];
+    for (let i = 0; i < days; i++) {
+        const date = d3.timeDay.offset(firstDay, i);
+        // Count number of games for each day
+        schedule.push({"date": date, "value": data.filter(d => d.day === i && !teams.has(d.team1) && !teams.has(d.team2)).length});
+    }
+    return schedule;
 }
 
 function drawCalendar() {
@@ -68,32 +86,11 @@ function drawCalendar() {
     const season = svg.append("g");
     season.attr("transform", `translate(50, ${cellSize * 1.5})`)
 
-    // season
-    //     .append("text")
-    //     .attr("x", 50)
-    //     .attr("y", -10)
-    //     .attr("text-anchor", "end")
-    //     .attr("font-size", 16)
-    //     .attr("font-weight", 550)
-    //     // .attr("transform", "rotate(270)")
-    //     .text("2015");
-
     var yearDiv = season.append("foreignObject")
         .attr("width", "100%")
         .attr("height", "100%")
         .attr("x", 0)
         .attr("y", -40)
-    //     .attr("class", "col-auto")
-    //
-    //
-    //
-    // yearDiv.append("xhtml:select")
-    //     .attr("class", "custom-select")
-    //     .selectAll("option")
-    //     .data([1, 2, 3])
-    //     .join("option")
-    //     .attr("value", d => d)
-    //     .text(d => d)
 
     const yearButton = yearDiv.append("xhtml:button")
         .attr("class", "btn btn-primary dropdown-toggle")
@@ -132,7 +129,6 @@ function drawCalendar() {
     const formatDay = d =>
         ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"][d];
 
-    const formatDate = d3.utcFormat("%x");
     colorFn.domain([0, maxValue]);
 
     season
@@ -194,13 +190,19 @@ function drawCalendar() {
     return season;
 }
 
+/** @param {D3 selection} schedule Selection to update */
 function drawFilter(schedule) {
+    let workingSet = new Set(workingSetAll);
+    let tmpSchedule = daySchedule(scheduleData, emptySet);
+    let teamSchedule = scheduleData.filter(d => (d.team1 === currentTeam.abbrv || d.team2 === currentTeam.abbrv));
+    let filterTeams = new Set();
+
     clear(collapseDiv);
     const filter = collapse
         .append("g")
-        // .attr("transform", `translate(10, ${yearHeight + cellSize * 2})`);
         .attr("transform", `translate(10, 0)`);
 
+    // Get logo files
     const rowLen = 6;
     const categories = teams.map(t => {
         return {
@@ -210,8 +212,8 @@ function drawFilter(schedule) {
         }
     });
 
+    // Draw logos
     const imageWidth = 60;
-
     const paddingX = 30;
     const paddingY = 15;
     filter
@@ -225,51 +227,73 @@ function drawFilter(schedule) {
         .attr("height", imageWidth)
         .on("click", legendClick);
 
+    // Handle filtering for each schedule variant
     function legendClick(data, i, nodes) {
         const {selected} = data;
         data.selected = !selected;
         nodes[i].style.opacity = data.selected ? 1 : 0.2;
-        console.log(svgToggle1.classList);
+        data.selected ? filterTeams.delete(data.abbrv) : filterTeams.add(data.abbrv);
 
         if (isActive(svgToggle1)) {
             // Season schedule
-            const change = data.selected ? g => {
-                if (workingSet.has(g)) {
-                    return false
-                } else {
-                    return workingSet.add(g);
-                }
-            } : workingSet.delete
-            const boundChange = change.bind(workingSet);
+            tmpSchedule = daySchedule(scheduleData, filterTeams);
+            let max = 0;
+            tmpSchedule.forEach(day => max = Math.max(max, day.value))
 
-            // "Dirty" indices
-            const teamArr = new Array()
-            scheduleData.forEach(g => {
-                if (g.team1 === data.abbrv || g.team2 === data.abbrv) {
-                    if (boundChange(g)) {
-                        teamArr.push(g)
-                    }
-                }
-            });
-            const teamDays = teamArr.map(g => finalSchedule[g.day]);
-            // Update number of games per day
-            data.selected ? teamDays.forEach(g => g.value++) : teamDays.forEach(g => g.value--);
-            let max = 0
-            for (let day in teamDays) {
-                max = Math.max(max, teamDays[day].value);
-            }
+            /*
+            // Cheaper but more complex to catch all cases
+            const change = data.selected ? g => {
+                 if (workingSet.has(g)) {
+                     return false
+                 } else {
+                     return workingSet.add(g);
+                 }
+             } : workingSet.delete
+             const boundChange = change.bind(workingSet);
+
+             // "Dirty" indices
+             const teamArr = new Array()
+             scheduleData.forEach(g => {
+                 if ((g.team1 === data.abbrv || g.team2 === data.abbrv) && boundChange(g)) {
+                     teamArr.push(g)
+                 }
+             });
+             const teamDays = teamArr.map(g => Object.assign(tmpSchedule[g.day]));
+             // Update number of games per day
+             data.selected ? teamDays.forEach(g => g.value++) : teamDays.forEach(g => g.value--);
+             let max = 0
+             for (let day in teamDays) {
+                 max = Math.max(max, teamDays[day].value);
+             }
+             */
+
             colorFn.domain([0, max + 1])
 
+            // Update color
             schedule.selectAll("rect")
-                .data(teamDays, d => d.date)
+                .data(tmpSchedule, d => d.date)
                 .transition()
                 .duration(500)
                 .attr('fill', d => colorFn(d.value));
+
+            // Update hover text
+            schedule.selectAll("title")
+                .data(tmpSchedule, d => d.date)
+                .text(d => `${formatDate(d.date)}: ${d.value}`);
+
+
         } else {
             // Team schedule
-            console.log(i);
-            console.log(data);
-            console.log(nodes[i])
+            let showTeams = teamSchedule.filter(d => !filterTeams.has(d.team1) && !filterTeams.has(d.team2));
+            schedule.selectAll("image")
+                .data(showTeams, d => teamSchedule.indexOf(d))
+                .join(
+                    enter => enter,
+                    update => update
+                        .style("opacity", 1),
+                    exit => exit
+                        .style("opacity", 0.2)
+                );
         }
     }
 
@@ -279,6 +303,7 @@ function drawFilter(schedule) {
 function drawTeamSchedule(team) {
     clear(svgDiv);
     const teamSchedule = scheduleData.filter(d => d.team1 === team.abbrv || d.team2 === team.abbrv);
+    console.log(teamSchedule);
     const rows = 4;
     const baseCellSize = 40;
     const cellSize = baseCellSize * 7 / rows;
@@ -293,7 +318,6 @@ function drawTeamSchedule(team) {
         .attr("text-anchor", "end")
         .attr("font-size", 16)
         .attr("font-weight", 550)
-        // .attr("transform", "rotate(270)")
         .text(team.abbrv);
 
     schedule
@@ -306,8 +330,6 @@ function drawTeamSchedule(team) {
         .attr("height", cellSize - 1.5)
         .attr("x", (d, i) => (i % Math.ceil(gamesPerTeam / rows)) * cellSize + 0.5)
         .attr("y", (d, i) => Math.floor(i / Math.ceil(gamesPerTeam / rows)) * cellSize + 10)
-        // .attr("data-toggle", "modal")
-        // .attr("data-target", "#scheduleModal")
         .append("title")
         .text((d, i) => `Game ${i}`);
 
@@ -327,9 +349,7 @@ function drawTeamSchedule(team) {
         .attr("stroke-width", 3)
         .attr("fill-opacity", 0);
 
-    // schedule.append("g")
     stretch(svg);
-
     return schedule;
 }
 
@@ -338,7 +358,7 @@ function swapCalendar(e) {
         if (e.target.id === "svgToggle1") {
             return false;
         }
-        const schedule = drawTeamSchedule(teams[0]);
+        const schedule = drawTeamSchedule(currentTeam);
         drawFilter(schedule);
     } else {
         if (e.target.id === "svgToggle2") {
