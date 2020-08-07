69f08d24d63c03608f7471860c8955d19526aa5d
jeremy-weiss <j.weiss@yale.edu>
2020-08-07 17:03:14 -0400

    Clean up graph coloring

--- scheduler/bipartite_color.py
+++ scheduler/bipartite_color.py
@@ -5,12 +5,6 @@ import sys
 WEEK_LEN = 7
 IN_FILE = sys.argv[1]
 OUT_FILE = sys.argv[2]
-BASE_EVEN_WEEK = [0, 2, 4, 6]
-BASE_ODD_WEEK = [1, 3, 5, 6]
-EVEN_WEEK = BASE_EVEN_WEEK + BASE_ODD_WEEK
-ODD_WEEK = BASE_ODD_WEEK + BASE_EVEN_WEEK
-# TODO list of all games, 0 if second half / 1 if first half
-# TODO 41 games in each half, 20/21 games
 
 class Edge:
 	def __init__(self, match):
@@ -29,57 +23,41 @@ def main():
 
 	matches = np.array(rows).astype(int)
 	weeks, teams = np.amax(matches, axis=0)[:2] + 1
-	weekly = {}
+	weekly = [None for i in range(weeks)]
 
 	for week in range(weeks):
-		chosen = [0 for i in range(WEEK_LEN)]
 		vertices = {v: [False] * WEEK_LEN for v in range(teams)}
 		edges = [Edge(match) for match in matches if match[0] == week]
 		random.shuffle(edges)
 		for edge in edges:
 			v1 = vertices[edge.match[1]]
 			v2 = vertices[edge.match[2]]
-			color = choose_color(v1, v2, chosen)
+			color = choose_color(v1, v2)
 			edge.color = color
 			v1[color] = v2[color] = True
 		weekly[week] = edges
-		print(chosen)
 
-	weekly = {week: sorted(weekly[week], key=lambda x: x.color) for week in weekly}
+	for week_edges in weekly:
+		week_edges.sort(key=lambda x: x.color)
 	with open(OUT_FILE, 'w') as f:
 		writer = csv.writer(f)
 		writer.writerow(['day', 'team1', 'team2'])
-		for week in weekly:
-			week_arr = weekly[week]
-			for edge in week_arr:
-				# # Messes up the sorted property of days
-				# if week % 2 == 0:
-				# 	base_week = EVEN_WEEK
-				# else:
-				# 	base_week = ODD_WEEK
-				# writer.writerow([WEEK_LEN*week + base_week[edge.color], edge.match[1], edge.match[2]])
+		for week, week_edges in enumerate(weekly):
+			for edge in week_edges:
 				writer.writerow([WEEK_LEN * week + edge.color, edge.match[1], edge.match[2]])
 
 
-def choose_color(v1, v2, chosen):
+# Randomly choose a color based on the available ones
+def choose_color(v1, v2):
 	candidates = []
 	for i in range(WEEK_LEN):
 		if v1[i] is v2[i] is False:
 			candidates.append(i)
-
+	# Algorithm should never fail (proof in README.md)
 	if not candidates:
 		raise AssertionError("Too many colors")
 	else:
 		return random.choice(candidates)
-		# min = float("inf")
-		# idx = 0
-		# for val in candidates:
-		# 	if val < min:
-		# 		idx = val
-		# 		min = val
-		# chosen[idx] += 1
-		# return idx
-
 
 if __name__ == '__main__':
 	main()
